2928eb1c33033cbea9166322887f35b6010ac75c
Adam-Marchew <adam.marchewka234@gmail.com>
2025-10-28 09:43:59 +0100

    Add more type of movies and function Click for switches

--- list-of-movies/src/app/main/main.ts
+++ list-of-movies/src/app/main/main.ts
@@ -13,6 +13,8 @@ export class Main {
   isCheckedSF: boolean = false;
   isCheckedRomance: boolean = false;
   isCheckedAnime: boolean = false;
+  isCheckedHistory: boolean = false;
+  isCheckedHorror: boolean = false;
 
   isHiddenSF: boolean = false;
   isHiddenAnime: boolean = false;
@@ -23,49 +25,97 @@ export class Main {
   selectionAll(){
     
     if(this.isCheckedAll == false){
-       this.isCheckedAnime = false;
-       this.isCheckedSF = false;
-       this.isCheckedRomance = false;
+      this.isCheckedAnime = false;
+      this.isCheckedSF = false;
+      this.isCheckedRomance = false;
+      this.isCheckedHistory = false;
+      this.isCheckedHorror = false;
 
-       this.isHiddenSF = false;
-       this.isHiddenAnime = false;
-       this.isHiddenRomance = false;
+      this.isHiddenSF = false;
+      this.isHiddenAnime = false;
+      this.isHiddenRomance = false;
+      this.isHiddenHistory = false;
+      this.isHiddenHorror = false;
     }
   }
 
   selectionSF(){
       if(this.isCheckedSF == false){
-         this.isHiddenSF = false;
-         this.isHiddenAnime = true;
-         this.isHiddenRomance = true;
+        this.isHiddenSF = false;
+        this.isHiddenAnime = true;
+        this.isHiddenRomance = true;
+        this.isHiddenHistory = true;
+        this.isHiddenHorror = true;
 
-         this.isCheckedAll = false;
-         this.isCheckedAnime = false;
-         this.isCheckedRomance = false;
+        this.isCheckedHistory = false;
+        this.isCheckedHorror = false;
+        this.isCheckedAll = false;
+        this.isCheckedAnime = false;
+        this.isCheckedRomance = false;
        }
   }
 
   selectionAnime(){
     if(this.isCheckedAnime == false){
-         this.isHiddenSF = true;
-         this.isHiddenRomance = true;
-         this.isHiddenAnime = false;
+        this.isHiddenSF = true;
+        this.isHiddenRomance = true;
+        this.isHiddenAnime = false;
+        this.isHiddenHistory = true;
+        this.isHiddenHorror = true;
 
-         this.isCheckedAll = false;
-         this.isCheckedRomance = false;
-         this.isCheckedSF = false;
+        this.isCheckedHistory = false;
+        this.isCheckedHorror = false;
+        this.isCheckedAll = false;
+        this.isCheckedRomance = false;
+        this.isCheckedSF = false;
        }
   }
 
   selectionRomance(){
     if(this.isCheckedRomance == false){
-         this.isHiddenSF = true;
-         this.isHiddenAnime = true;
-         this.isHiddenRomance = false;
+        this.isHiddenSF = true;
+        this.isHiddenAnime = true;
+        this.isHiddenRomance = false;
+        this.isHiddenHistory = true;
+        this.isHiddenHorror = true;
 
-         this.isCheckedAll = false;
-         this.isCheckedAnime = false;
-         this.isCheckedSF = false;
+        this.isCheckedHistory = false;
+        this.isCheckedHorror = false;
+        this.isCheckedAll = false;
+        this.isCheckedAnime = false;
+        this.isCheckedSF = false;
+       }
+  }
+
+  selectionHistory(){
+      if(this.isCheckedHistory == false){
+        this.isHiddenSF = true;
+        this.isHiddenAnime = true;
+        this.isHiddenRomance = true;
+        this.isHiddenHistory = false;
+        this.isHiddenHorror = true;
+
+        this.isCheckedRomance = false;
+        this.isCheckedHorror = false;
+        this.isCheckedAll = false;
+        this.isCheckedAnime = false;
+        this.isCheckedSF = false;
+       }
+  }
+
+  selectionHorror(){
+      if(this.isCheckedHorror == false){
+        this.isHiddenSF = true;
+        this.isHiddenAnime = true;
+        this.isHiddenRomance = true;
+        this.isHiddenHistory = true;
+        this.isHiddenHorror = false;
+
+        this.isCheckedHistory = false;
+        this.isCheckedRomance = false;
+        this.isCheckedAll = false;
+        this.isCheckedAnime = false;
+        this.isCheckedSF = false;
        }
   }
 
